a93e08d8f5cb4e8fb745369aa85b6e5e5a4b463e
Rasmus Lindholm <rasmuslindholm@live.com>
2024-01-17 16:48:06 +0100

    adding new categories to my todo list with a new window with a new goal to make the user able to type a text in the new category

---
+++ Todo List/Details.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  Todo List
+//
+//  Created by Rasmus Lindholm on 2024-01-17.
+//
+
+import SwiftUI
+
+struct Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Details()
+}
